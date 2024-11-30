296f9119ab279337b9165b13b9d7025d5d956636
rawwaaannn <rawan.alaa1175@gmail.com>
2024-11-30 19:13:59 +0200

    Update MobileUsersController.cs

--- DrugKitAPI.API/Controllers/MobileUsersController.cs
+++ DrugKitAPI.API/Controllers/MobileUsersController.cs
@@ -1,11 +1,58 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DrugKitAPI.Core.Models;
+using System;
+using DrugKitAPI.EF.Data;
+using Microsoft.AspNetCore.Identity;
 
-namespace DrugKitAPI.API.Controllers
+
+namespace DrugKitAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet]
+        public async Task<ActionResult<List<MobileUser>>> GetMobileUsers()
+        {
+
+            var roleName = "User";
+
+
+            var userRoleId = await _context.Roles
+                .Where(r => r.Name == roleName)
+                .Select(r => r.Id)
+                .FirstOrDefaultAsync();
+
+
+
+            var mobileUsers = await _context.MobileUsers
+                .Include(mu => mu.ApplicationUser)  
+                .Include(mu => mu.Reports)         
+                .Where(mu => mu.ApplicationUser != null &&
+                             _context.UserRoles.Any(ur => ur.UserId == mu.ApplicationUser.Id && ur.RoleId == userRoleId)) 
+                .Select(mu => new
+                {
+                    UserId = mu.ApplicationUserId,
+                    UserName = mu.ApplicationUser.Name,
+                    UserEmail = mu.ApplicationUser.Email,
+                    ReportsCount = mu.Reports.Count
+                })
+                .ToListAsync();
+
+            return Ok(mobileUsers);
+        }
     }
 }
+
+
+
+
